d2fc461d1fca5eda57fd09b48d576ce9eef254ab
13621 <test@example.com>
2024-08-31 20:44:51 +0200

    remove debug variable in gha

--- scripts/gha_build_push.sh
+++ scripts/gha_build_push.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-repo=13621/jmusicbot-docker
-
 OCI_ARCHIVE=$(nix build --print-out-paths)
 
 nix run nixpkgs#skopeo -- --insecure-policy copy "docker-archive:${OCI_ARCHIVE}" "docker://ghcr.io/${repo}"
