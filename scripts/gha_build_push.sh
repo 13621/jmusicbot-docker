5f99caf50d65f9c7605ff4d2e0e8cac91851c405
13621 <test@example.com>
2024-09-01 11:27:57 +0200

    feat: use nix-shell shebang

--- scripts/gha_build_push.sh
+++ scripts/gha_build_push.sh
@@ -1,8 +1,9 @@
-#! /bin/sh
+#! /usr/bin/env nix-shell
+#! nix-shell -p skopeo -i bash
 
 set -e
 
 OCI_ARCHIVE=$(nix build --print-out-paths)
 
-nix run nixpkgs#skopeo -- --insecure-policy copy "docker-archive:${OCI_ARCHIVE}" "docker://ghcr.io/${repo}"
-nix run nixpkgs#skopeo -- --insecure-policy copy "docker-archive:${OCI_ARCHIVE}" "docker://ghcr.io/${repo}:latest"
+skopeo --insecure-policy copy "docker-archive:${OCI_ARCHIVE}" "docker://ghcr.io/${repo}"
+skopeo --insecure-policy copy "docker-archive:${OCI_ARCHIVE}" "docker://ghcr.io/${repo}:latest"
